98741020679b56517ee3a07daeecd6a1a45262b8
Eric Steinborn <esteinborn@gmail.com>
2013-11-25 16:34:56 -0500

    Adding Content

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,8 @@ module.exports = function(grunt) {
     sass: {
       dist: {
         options: {
-          includePaths: ['scss']
+          includePaths: ['scss'],
+          sourceComments: 'normal'
         },
         files: {
           'dist/assets/css/foundation.css': '<%= foundation.scss %>',
@@ -80,50 +81,10 @@ module.exports = function(grunt) {
         ]
       }
     },
-
     clean: ['dist/'],
-
-    // karma: {
-    //   options: {
-    //     configFile: 'karma.conf.js',
-    //     runnerPort: 9999,
-    //   },
-    //   continuous: {
-    //     singleRun: true,
-    //     browsers: ['TinyPhantomJS', 'SmallPhantomJS']
-    //   },
-    //   dev: {
-    //     singleRun: true,
-    //     browsers: ['TinyPhantomJS', 'SmallPhantomJS', 'TinyChrome', 'Firefox'],
-    //     reporters: 'dots'
-    //   },
-    //   dev_watch: {
-    //     background: true,
-    //     browsers: ['TinyPhantomJS', 'SmallPhantomJS', 'TinyChrome', 'Firefox']
-    //   },
-    //   mac: {
-    //     singleRun: true,
-    //     browsers: ['TinyPhantomJS', 'SmallPhantomJS', 'TinyChrome', 'Firefox', 'Safari'],
-    //     reporters: 'dots'
-    //   },
-    //   win: {
-    //     singleRun: true,
-    //     browsers: ['TinyPhantomJS', 'SmallPhantomJS', 'TinyChrome', 'Firefox', 'IE'],
-    //     reporters: 'dots'
-    //   }
-    // },
 
     watch_start: {
       grunt: { files: ['Gruntfile.js'] },
-      // karma: {
-      //   files: [
-      //     'dist/assets/js/*.js',
-      //     'spec/**/*.js',
-      //     'dist/assets/css/*.css'
-      //   ],
-      //   tasks: ['karma:dev_watch:run']
-      // },
-
       styles: {
         files: ['scss/**/*.scss', 'doc/assets/**/*.scss'],
         tasks: ['sass'],
@@ -160,8 +121,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compress');
-  // grunt.loadNpmTasks('grunt-karma');
-  // grunt.loadNpmTasks('grunt-rsync');
   grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-newer');
 
@@ -172,6 +131,4 @@ module.exports = function(grunt) {
   grunt.registerTask('compile', ['compile:assets', 'assemble']);
   grunt.registerTask('build', ['compile', 'compress']);
   grunt.registerTask('default', ['compile', 'watch']);
-  // grunt.registerTask('travis', ['compile', 'karma:continuous']);
-  // grunt.registerTask('deploy', ['compile', 'rsync:dist']);
 };
